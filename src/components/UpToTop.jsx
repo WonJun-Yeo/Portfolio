ee71e9ce863a47e0a84df1d010ad70b17c27cc4c
wonjun.yeo <rbgh004@gamil.com>
2023-06-14 15:52:58 +0900

    add sectioning components

---
+++ src/components/UpToTop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpToTop = () => {
+  return (
+      <div>
+
+      </div>
+  );
+};
+
+export default UpToTop;
---
+++ src/components/main/Sills.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sills = () => {
+  return (
+      <div>
+
+      </div>
+  );
+};
+
+export default Sills;
