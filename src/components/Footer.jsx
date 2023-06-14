284343b71f54d92e688e552d2ea0cd75cb9904b5
wonjun.yeo <rbgh004@gamil.com>
2023-06-14 19:51:54 +0900

    add Footer markup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import Contact from "./main/Contact";
 
 const Footer = () => {
   return (
-      <div>
-
-      </div>
+      <footer>
+        <Contact />
+        <p>RubisCo - All rights reserved</p>
+      </footer>
   );
 };
 
