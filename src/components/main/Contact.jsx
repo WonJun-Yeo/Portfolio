01b84ed0c68df179129af72e0a8b483c810dfeb7
wonjun.yeo <rbgh004@gamil.com>
2023-06-14 19:51:36 +0900

    add Contact markup

--- src/components/main/Contact.jsx
+++ src/components/main/Contact.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
 
 const Contact = () => {
   return (
-      <div>
-
-      </div>
+      <>
+        <h2>Let&#39;s talk</h2>
+        <p>rbgh004@naver.com</p>
+        <ul>
+          <li>
+            <a href="https://github.com/WonJun-Yeo" title={`WonJun's github link`} target={'_blank'}>
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/WonJun-Yeo">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+        </ul>
+      </>
   );
 };
 
