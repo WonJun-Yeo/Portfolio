b0a649b068ea7cbf35aeecf698a5773d8bb9dca4
wonjun.yeo <rbgh004@gamil.com>
2023-06-14 20:44:02 +0900

    add css header.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import '../styles/header.css';
 import logo from '../assets/images/favicon.ico';
 
 const Header = () => {
   return (
-      <header>
-        <div>
-          <img src={logo} alt="logo"/>
-          <h1><a href="#">WonJun</a></h1>
+      <header className={'header'}>
+        <div className={'header_logo'}>
+          <img src={logo} alt="logo" className={'header_logo_img'}/>
+          <h1 className={'header_logo_title'}><a href="#">WonJun</a></h1>
         </div>
         <nav>
-          <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#skiils">Skills</a></li>
-            <li><a href="#works">Works</a></li>
-            <li><a href="#contact">Contact</a></li>
+          <ul className={'header_menu'}>
+            <li><a className={'header_menu_item active'} href="#home">Home</a></li>
+            <li><a className={'header_menu_item'} href="#about">About</a></li>
+            <li><a className={'header_menu_item'} href="#skiils">Skills</a></li>
+            <li><a className={'header_menu_item'} href="#works">Works</a></li>
+            <li><a className={'header_menu_item'} href="#contact">Contact</a></li>
           </ul>
         </nav>
       </header>
