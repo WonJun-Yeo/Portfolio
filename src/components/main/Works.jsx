19f2927213a6321e270e476f424ecfd17fd07b16
wonjun.yeo <rbgh004@gamil.com>
2023-06-14 19:39:54 +0900

    add Works markup

--- src/components/main/Works.jsx
+++ src/components/main/Works.jsx
@@ -2,9 +2,68 @@ import React from 'react';
 
 const Works = () => {
   return (
-      <div>
-        
-      </div>
+      <section id={'works'}>
+        <h2>My Works</h2>
+        <p>Projects</p>
+        <ul>
+          <li>
+            <button>All <span>8</span></button>
+          </li>
+          <li>
+            <button>Front-end <span>3</span></button>
+          </li>
+          <li>
+            <button>Back-end <span>3</span></button>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#" target={'_blank'}>
+              <img src="" alt="project1_img"/>
+              <div>
+                <h3>Project #1</h3>
+                <p>Clone Coding with HTML, CSS</p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" target={'_blank'}>
+              <img src="" alt="project1_img"/>
+              <div>
+                <h3>Project #1</h3>
+                <p>Clone Coding with HTML, CSS</p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" target={'_blank'}>
+              <img src="" alt="project1_img"/>
+              <div>
+                <h3>Project #1</h3>
+                <p>Clone Coding with HTML, CSS</p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" target={'_blank'}>
+              <img src="" alt="project1_img"/>
+              <div>
+                <h3>Project #1</h3>
+                <p>Clone Coding with HTML, CSS</p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" target={'_blank'}>
+              <img src="" alt="project1_img"/>
+              <div>
+                <h3>Project #1</h3>
+                <p>Clone Coding with HTML, CSS</p>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </section>
   );
 };
 
