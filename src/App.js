9b1daa8998711ab0c346dd66f931fb3736c3f85a
wonjun.yeo <rbgh004@gamil.com>
2023-06-14 16:58:28 +0900

    add Home markup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./components/main/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import profile from '../../assets/images/profile.jpg';
 
 const Home = () => {
   return (
-      <div>
-        
-      </div>
+      <section id={'home'}>
+        <img src={profile} alt="WonJun's profile"/>
+        <h2>Hello I&#39;m RubisCo, WonHun</h2>
+        <p>나는 무슨 소프트웨어 엔지니어 입니다.</p>
+        <a href="#contact">Contact Me</a>
+      </section>
   );
 };
 
