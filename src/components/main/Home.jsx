c1def3509dfb1e4c2949739258aa1f941b95b728
wonjun.yeo <rbgh004@gamil.com>
2023-06-14 23:51:22 +0900

    add home.css

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import profile from '../../assets/images/profile.jpg';
+import '../../styles/home.css'
+import profile from '../../assets/images/profile.png';
 
 const Home = () => {
   return (
       <section id={'home'}>
-        <img src={profile} alt="WonJun's profile"/>
-        <h2>Hello I&#39;m RubisCo, WonHun</h2>
-        <p>나는 무슨 소프트웨어 엔지니어 입니다.</p>
-        <a href="#contact">Contact Me</a>
+        <img src={profile} alt="WonJun's profile" className={'home_avatar'} />
+        <h2 className={'home_title'}>Hello <br />I&#39;m <strong className={'home_title_strong'}>RubisCo</strong>, WonJun</h2>
+        <p className={'home_description'}>Hello나는 무슨 소프트웨어 엔지니어 입니다.</p>
+        <a href="#contact" className={'home_contact'}>Contact Me</a>
       </section>
   );
 };
