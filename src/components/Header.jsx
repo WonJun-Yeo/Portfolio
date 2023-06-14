d0b601642b69a7a918706ec35943bb1f060ae395
wonjun.yeo <rbgh004@gamil.com>
2023-06-14 16:49:17 +0900

    add header markup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import logo from '../assets/images/favicon.ico';
 
 const Header = () => {
   return (
-      <div>
-
-      </div>
+      <header>
+        <div>
+          <img src={logo} alt="logo"/>
+          <h1><a href="#">WonJun</a></h1>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#skiils">Skills</a></li>
+            <li><a href="#works">Works</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
   );
 };
 
