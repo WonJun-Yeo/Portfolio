1787d72d7eb923dc65a0b443566a841652062b89
wonjun.yeo <rbgh004@gamil.com>
2023-06-14 20:23:08 +0900

    add css setup files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import './App.css';
+import './styles/reset.css';
+import './styles/init.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/main/Home";
+import About from "./components/main/About";
+import Skills from "./components/main/Skills";
+import Works from "./components/main/Works";
+import UpToTop from "./components/UpToTop";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <About />
+      <Skills />
+      <Works />
+      <UpToTop />
       <Footer />
     </div>
   );
