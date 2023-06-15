7036da9c5b18167ae9c473137244846b652eb977
wonjun.yeo <rbgh004@gamil.com>
2023-06-15 17:10:46 +0900

    add filter click event to Works section

--- src/components/main/Works.jsx
+++ src/components/main/Works.jsx
@@ -3,6 +3,32 @@ import '../../styles/main/works.css';
 import test_image from '../../assets/images/test_image.jpg';
 
 const Works = () => {
+  const handleOnClickCategory = (e) => {
+    //TODO: modify based mockdata
+    const filter = e.target.dataset.category;
+    if (!filter) return;
+
+    const active = document.querySelector('.category_selected');
+    active.classList.remove('category_selected');
+    e.target.classList.add('category_selected');
+
+    const projects = document.querySelectorAll('.project');
+    const projectsContainer = document.querySelector('.projects');
+
+    projectsContainer.classList.add('anim-out');
+
+    setTimeout(() => {
+      projectsContainer.classList.remove('anim-out');
+      projects.forEach(project => {
+        if(filter === 'all' || filter === project.dataset.type) {
+          project.style.display = 'block';
+        } else {
+          project.style.display = 'none';
+        }
+      });
+    }, 150);
+  };
+
   return (
       <section id={'works'} className={'section'}>
         <div className={'max_container'}>
@@ -10,18 +36,18 @@ const Works = () => {
           <p className={'description'}>Projects</p>
           <ul className={'categories'}>
             <li>
-              <button className={'category category_selected'}>All <span className={'category_count'}>8</span></button>
+              <button className={'category category_selected'} data-category={'all'} onClick={handleOnClickCategory}>All <span className={'category_count'}>6</span></button>
             </li>
             <li>
-              <button className={'category'}>Front-end <span className={'category_count'}>3</span></button>
+              <button className={'category'} data-category={'front-end'} onClick={handleOnClickCategory}>Front-end <span className={'category_count'}>3</span></button>
             </li>
             <li>
-              <button className={'category'}>Back-end <span className={'category_count'}>3</span></button>
+              <button className={'category'} data-category={'back-end'} onClick={handleOnClickCategory}>Back-end <span className={'category_count'}>3</span></button>
             </li>
           </ul>
           <ul className={'projects'}>
-            <li className={'project'}>
-              <a href="#" target={'_blank'}>
+            <li className={'project'} data-type={'back-end'}>
+              <a href="/#" target={'_blank'}>
                 <img src={test_image} alt="project1_img" className={'project_image'}/>
                 <div className={'project_metadata'}>
                   <h3 className={'project_title'}>Project #1</h3>
@@ -29,8 +55,8 @@ const Works = () => {
                 </div>
               </a>
             </li>
-            <li className={'project'}>
-              <a href="#" target={'_blank'}>
+            <li className={'project'} data-type={'back-end'}>
+              <a href="/#" target={'_blank'}>
                 <img src={test_image} alt="project1_img" className={'project_image'}/>
                 <div className={'project_metadata'}>
                   <h3 className={'project_title'}>Project #1</h3>
@@ -38,8 +64,8 @@ const Works = () => {
                 </div>
               </a>
             </li>
-            <li className={'project'}>
-              <a href="#" target={'_blank'}>
+            <li className={'project'} data-type={'front-end'}>
+              <a href="/#" target={'_blank'}>
                 <img src={test_image} alt="project1_img" className={'project_image'}/>
                 <div className={'project_metadata'}>
                   <h3 className={'project_title'}>Project #1</h3>
@@ -47,8 +73,8 @@ const Works = () => {
                 </div>
               </a>
             </li>
-            <li className={'project'}>
-              <a href="#" target={'_blank'}>
+            <li className={'project'} data-type={'back-end'}>
+              <a href="/#" target={'_blank'}>
                 <img src={test_image} alt="project1_img" className={'project_image'}/>
                 <div className={'project_metadata'}>
                   <h3 className={'project_title'}>Project #1</h3>
@@ -56,8 +82,8 @@ const Works = () => {
                 </div>
               </a>
             </li>
-            <li className={'project'}>
-              <a href="#" target={'_blank'}>
+            <li className={'project'} data-type={'front-end'}>
+              <a href="/#" target={'_blank'}>
                 <img src={test_image} alt="project1_img" className={'project_image'}/>
                 <div className={'project_metadata'}>
                   <h3 className={'project_title'}>Project #1</h3>
@@ -65,8 +91,8 @@ const Works = () => {
                 </div>
               </a>
             </li>
-            <li className={'project'}>
-              <a href="#" target={'_blank'}>
+            <li className={'project'} data-type={'front-end'}>
+              <a href="/#" target={'_blank'}>
                 <img src={test_image} alt="project1_img" className={'project_image'}/>
                 <div className={'project_metadata'}>
                   <h3 className={'project_title'}>Project #1</h3>
@@ -74,8 +100,8 @@ const Works = () => {
                 </div>
               </a>
             </li>
-            <li className={'project'}>
-              <a href="#" target={'_blank'}>
+            <li className={'project'} data-type={'front-end'}>
+              <a href="/#" target={'_blank'}>
                 <img src={test_image} alt="project1_img" className={'project_image'}/>
                 <div className={'project_metadata'}>
                   <h3 className={'project_title'}>Project #1</h3>
@@ -83,43 +109,6 @@ const Works = () => {
                 </div>
               </a>
             </li>
-            <li className={'project'}>
-              <a href="#" target={'_blank'}>
-                <img src={test_image} alt="project1_img" className={'project_image'}/>
-                <div className={'project_metadata'}>
-                  <h3 className={'project_title'}>Project #1</h3>
-                  <p>Clone Coding with HTML, CSS</p>
-                </div>
-              </a>
-            </li>
-            <li className={'project'}>
-              <a href="#" target={'_blank'}>
-                <img src={test_image} alt="project1_img" className={'project_image'}/>
-                <div className={'project_metadata'}>
-                  <h3 className={'project_title'}>Project #1</h3>
-                  <p>Clone Coding with HTML, CSS</p>
-                </div>
-              </a>
-            </li>
-            <li className={'project'}>
-              <a href="#" target={'_blank'}>
-                <img src={test_image} alt="project1_img" className={'project_image'}/>
-                <div className={'project_metadata'}>
-                  <h3 className={'project_title'}>Project #1</h3>
-                  <p>Clone Coding with HTML, CSS</p>
-                </div>
-              </a>
-            </li>
-            <li className={'project'}>
-              <a href="#" target={'_blank'}>
-                <img src={test_image} alt="project1_img" className={'project_image'}/>
-                <div className={'project_metadata'}>
-                  <h3 className={'project_title'}>Project #1</h3>
-                  <p>Clone Coding with HTML, CSS</p>
-                </div>
-              </a>
-            </li>
-
           </ul>
         </div>
       </section>
