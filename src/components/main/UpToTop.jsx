1ef99c31a6983be719b550e13262016bc79bb394
wonjun.yeo <rbgh004@gamil.com>
2023-06-15 12:17:19 +0900

    add uptotop.css

--- src/components/main/UpToTop.jsx
+++ src/components/main/UpToTop.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../../styles/main/uptotop.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowUp} from "@fortawesome/free-solid-svg-icons";
 
 const UpToTop = () => {
   return (
       <aside>
-        <a href="src/components#home" title={'up to top'}>
+        <a href="src/components#home" title={'up to top'} className={'arrow-up'}>
           <FontAwesomeIcon icon={faArrowUp} />
         </a>
       </aside>
