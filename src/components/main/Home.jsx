92dd09665f64a6fa0c1b48ee8d01546cbdee87da
wonjun.yeo <rbgh004@gamil.com>
2023-06-19 13:33:56 +0900

    fix home image styles

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -13,23 +13,26 @@ const Home = () => {
 
   return (
       <section id={'home'}>
-        <div className={'home_container'}>
+        <div className={'max_container home_container'}>
           <img src={profile} alt="WonJun's profile" className={'home_avatar'}/>
-          <h2 className={'home_title'}>안녕하세요! <br/>저는
-            <TypeIt className={'home_title_strong'}
-                    element={"strong"}
-                    getBeforeInit={(instance) => {
-                      instance.type(" FE&BE ").pause(750).delete(10).pause(500).type(" 그런 노력을 ");
+          <div>
+            <h2 className={'home_title'}>안녕하세요! <br/>저는
+              <TypeIt className={'home_title_strong'}
+                      element={"strong"}
+                      getBeforeInit={(instance) => {
+                        instance.type(" FE&BE ").pause(750).delete(10).pause(500).type(" 그런 노력을 ");
 
-                      return instance;
-                    }}
-            />
-            다하는 , 원준 입니다.</h2>
-          <p className={'home_description'}>Hello나는 무슨 소프트웨어 엔지니어 입니다.</p>
-          <a href="#footer" className={'home_contact'}>Contact Me</a>
+                        return instance;
+                      }}
+              />
+              다하는 , 원준 입니다.</h2>
+            <p className={'home_description'}>Hello나는 무슨 소프트웨어 엔지니어 입니다.</p>
+            <a href="#footer" className={'home_contact'}>Contact Me</a>
+          </div>
         </div>
         <div className="home-divider">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
+               preserveAspectRatio="none">
             <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill"></path>
           </svg>
         </div>
