e4f850cf593a361c147e64947431a593f9bc0abd
wonjun.yeo <rbgh004@gamil.com>
2023-06-19 13:16:14 +0900

    apply typing styling to home title

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import '../../styles/main/home.css'
 import profile from '../../assets/images/profile.png';
+import TypeIt from "typeit-react";
 
 const Home = () => {
 
   document.addEventListener('scroll', () => {
     const homeContainer = document.querySelector('.home_container');
     const homeContainerHeight = homeContainer.getBoundingClientRect().height;
-    homeContainer.style.opacity = 1- window.scrollY / homeContainerHeight;
+    homeContainer.style.opacity = 1 - window.scrollY / homeContainerHeight;
   });
 
   return (
       <section id={'home'}>
         <div className={'home_container'}>
-          <img src={profile} alt="WonJun's profile" className={'home_avatar'} />
-          <h2 className={'home_title'}>Hello <br />I&#39;m <strong className={'home_title_strong'}>RubisCo</strong>, WonJun</h2>
+          <img src={profile} alt="WonJun's profile" className={'home_avatar'}/>
+          <h2 className={'home_title'}>안녕하세요! <br/>저는
+            <TypeIt className={'home_title_strong'}
+                    element={"strong"}
+                    getBeforeInit={(instance) => {
+                      instance.type(" FE&BE ").pause(750).delete(10).pause(500).type(" 그런 노력을 ");
+
+                      return instance;
+                    }}
+            />
+            다하는 , 원준 입니다.</h2>
           <p className={'home_description'}>Hello나는 무슨 소프트웨어 엔지니어 입니다.</p>
           <a href="#footer" className={'home_contact'}>Contact Me</a>
         </div>
